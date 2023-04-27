26ab7996394c8dac1bac2b7d2e592706dcc3a612
Terrywu0208 <75594915+Terrywu0208@users.noreply.github.com>
2023-04-27 12:55:40 +0800

    v4 m7

--- v4/financialdata/tasks.py
+++ v4/financialdata/tasks.py
@@ -11,6 +11,6 @@ def crawler(dataset: str,parameters: typing.Dict[str, str],):
     # 使用 getattr, importlib,
     # 根據不同 dataset, 使用相對應的 crawler 收集資料
     # 爬蟲
-    df = getattr(importlib.import_module(f"crawler.{dataset}"),"crawler",)(parameters=parameters)
+    df = getattr(importlib.import_module(f"./crawler/{dataset}.py"),"crawler",)(parameters=parameters)
     print(df)
     print("upload db")
