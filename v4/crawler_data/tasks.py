ccecef39b8d6cf3c78110c973230e1d3548c4058
Terrywu0208 <75594915+Terrywu0208@users.noreply.github.com>
2023-05-15 14:17:36 +0800

    20230515_m7

--- v4/crawler_data/tasks.py
+++ v4/crawler_data/tasks.py
@@ -4,7 +4,8 @@ from datetime import datetime
 from worker import app
 import crawler_pk.udn_news_crawler as nc
 import crawler_pk.weather_crawler as wc
-import db.db
+import db
+
 
 # 註冊 task, 有註冊的 task 才可以變成任務發送給 rabbitmq
 @app.task()
@@ -14,7 +15,7 @@ def crawler(dataset: str,parameters: typing.Dict[str, str],):
         df = nc.news_crawler(time_format)
     elif dataset == "weather_crawler":
         df = wc.weather_crawler(time_format)
-    db.upload_data(df, dataset, db.router.mysql_financialdata_conn)
+    db.db.upload_data(df, dataset, db.router.mysql_financialdata_conn)
     print("crawler_date : ",parameters['crawler_date'])
     print(df)
     print("crawler")
